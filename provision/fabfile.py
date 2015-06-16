7a969e1d410272c14080716313a72717a3f51e29
swasher <mr.swasher@gmail.com>
2015-06-16 16:26:31 +0300

    minor ansible changes

--- provision/fabfile.py
+++ provision/fabfile.py
@@ -1,10 +1,15 @@
 from fabric.api import local, hosts, env, run
+from fabric.context_managers import cd
+
 
 def provision_staging():
-    local('ansible-playbook -i inventories/staging --ask-become-pass --ask-vault-pass provision.yml')
+    local('ansible-playbook -i inventories/staging --ask-become-pass provision.yml')
+
+def provision_staging_retry():
+    local('ansible-playbook -i inventories/staging --ask-become-pass --limit @/home/vagrant/provision.retry provision.yml')
 
 def provision_production():
-    local('ansible-playbook -i inventories/production --ask-become-pass -vv --ask-vault-pass provision.yml')
+    local('ansible-playbook -i inventories/production --ask-become-pass -vv provision.yml')
 
 def provision_local():
     #local('ansible-playbook -i inventories/vagrant -v provision.yml ')
@@ -22,12 +27,15 @@ env.use_ssh_config = True
 
 def staging():
     env.hosts = ['staging']
+    env.project_path = '/home/swasher/pdfupload'
 
 def production():
     env.hosts = ['production']
 
 def deploy():
-    run('uname -a')
-    run('pwd')
-    #run('git fetch origin')
-    #run('git reset --hard origin/master')
+    with cd(env.project_path):
+        run('uname -a')
+        run('ll')
+        run('pwd')
+        #run('git fetch origin')
+        #run('git reset --hard origin/master')

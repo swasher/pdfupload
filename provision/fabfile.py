ddf6c925a6f7f2712885390c81fa3c231b2b1bb2
swasher <mr.swasher@gmail.com>
2015-06-22 11:36:20 +0300

    minor ansible changes

--- provision/fabfile.py
+++ provision/fabfile.py
@@ -1,19 +1,17 @@
 from fabric.api import local, hosts, env, run
 from fabric.context_managers import cd
 
-
-def provision_staging():
-    local('ansible-playbook -i inventories/staging --ask-become-pass provision.yml')
-
-def provision_staging_retry():
-    local('ansible-playbook -i inventories/staging --ask-become-pass --limit @/home/vagrant/provision.retry provision.yml')
-
-def provision_production():
-    local('ansible-playbook -i inventories/production --ask-become-pass -vv provision.yml')
-
-def provision_local():
-    #local('ansible-playbook -i inventories/vagrant -v provision.yml ')
-    local('ansible-playbook -i inventories/vagrant -vv --skip-tags=vagrant_skip provision.yml ')
+# ####### DPRECATED
+# def provision_staging():
+#     local('ansible-playbook -i inventories/staging --ask-become-pass provision.yml')
+#
+# def provision_production():
+#     local('ansible-playbook -i inventories/production --ask-become-pass -vv provision.yml')
+#
+# def provision_local():
+#     #local('ansible-playbook -i inventories/vagrant -v provision.yml ')
+#     local('ansible-playbook -i inventories/vagrant -vv --skip-tags=vagrant_skip provision.yml ')
+#######################
 
 # For use this feature, you must have hosts description at ansible machine in ~/.shh/config with following format:
 # Host staging
@@ -23,14 +21,29 @@ def provision_local():
 #
 # Then we can us deploy as `fab staging deploy` or `fab production deploy`
 
+
+# USAGE
+# fab [staging|production|vagrant] [provision|deploy]
+#
+
 env.use_ssh_config = True
+env.project_path = '/home/swasher/pdfupload'
+
 
 def staging():
     env.hosts = ['staging']
-    env.project_path = '/home/swasher/pdfupload'
 
 def production():
     env.hosts = ['production']
+
+def vagrant():
+    env.hosts = ['vagrant']
+
+
+def provision():
+    additional_params = '--skip-tags=vagrant_skip' if env.hosts == 'vagrant' else ''
+    local('ansible-playbook -i inventories/{machine} --ask-become-pass -v {additional_params} provision.yml'.format(machine=env.hosts[0], additional_params=additional_params))
+
 
 def deploy():
     with cd(env.project_path):

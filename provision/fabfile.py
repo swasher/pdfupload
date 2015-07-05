807226d3850591872a4ba442226e0d9476058d21
swasher <mr.swasher@gmail.com>
2015-07-05 20:29:33 +0300

    tunung path

--- provision/fabfile.py
+++ provision/fabfile.py
@@ -1,19 +1,8 @@
 from fabric.api import local, hosts, env, run
 from fabric.context_managers import cd
+from fabric.api import settings
 
-# ####### DPRECATED
-# def provision_staging():
-#     local('ansible-playbook -i inventories/staging --ask-become-pass provision.yml')
-#
-# def provision_production():
-#     local('ansible-playbook -i inventories/production --ask-become-pass -vv provision.yml')
-#
-# def provision_local():
-#     #local('ansible-playbook -i inventories/vagrant -v provision.yml ')
-#     local('ansible-playbook -i inventories/vagrant -vv --skip-tags=vagrant_skip provision.yml ')
-#######################
-
-# For use this feature, you must have hosts description at ansible machine in ~/.shh/config with following format:
+# For use server names, you must have hosts description at ansible machine in ~/.shh/config with following format:
 # Host staging
 #     HostName 111.222.333.444
 #     Port 22000
@@ -28,6 +17,7 @@ from fabric.context_managers import cd
 
 env.use_ssh_config = True
 env.project_path = '/home/swasher/pdfupload'
+
 
 
 def staging():
@@ -58,6 +48,7 @@ def deploy():
     with cd(env.project_path):
         run('git fetch origin')
         run('git reset --hard origin/master')
+        run('sudo pip install -r requirements.txt')
         run('python manage.py migrate')
         run('touch /tmp/pdfupload.reload')
 
@@ -65,3 +56,45 @@ def deploy():
         #run('python manage.py test myapp')
 
         # also, we need reinstall requirements.txt due to new package may be used
+
+@hosts('production')
+def staging_restore():
+    env.hosts = ['staging']
+    #with settings(user='swasher', host_string='192.168.0.203', port='22522'):
+    #env.hosts = ['production']
+
+    run('hostname -f')
+    staging()
+    run('echo {}')
+    print(env.port)
+
+    with settings(hosts='staging'):
+        run('hostname -f')
+
+def parsing():
+    from os.path import expanduser
+    from paramiko.config import SSHConfig
+
+    def hostinfo(host, config):
+        hive = config.lookup(host)
+        if 'hostname' in hive:
+            host = hive['hostname']
+        if 'user' in hive:
+            host = '%s@%s' % (hive['user'], host)
+        if 'port' in hive:
+            host = '%s:%s' % (host, hive['port'])
+        return host
+
+    try:
+        config_file = file(expanduser('~/.ssh/config'))
+    except IOError, e:
+        print(e)
+    else:
+        config = SSHConfig()
+        config.parse(config_file)
+        keys = [config.lookup(host).get('identityfile', None) for host in env.hosts]
+        env.key_filename = [expanduser(key) for key in keys if key is not None]
+        env.hosts = [hostinfo(host, config) for host in env.hosts]
+
+        for role, rolehosts in env.roledefs.items():
+            env.roledefs[role] = [hostinfo(host, config) for host in rolehosts]

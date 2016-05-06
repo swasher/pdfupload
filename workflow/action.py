43169348f165b3039c2f5df773fdecd3345602cf
swasher <mr.swasher@gmail.com>
2016-05-06 12:03:20 +0300

    small fixes

--- workflow/action.py
+++ workflow/action.py
@@ -377,7 +377,7 @@ def send_telegram(pdf):
     if pdf.upload_to_ctpbureau_status:
 
         chat_id = settings.TELEGRAM_CHAT_ID
-        bot = TelegramBot(settings.telegram_api)
+        bot = TelegramBot(settings.TELEGRAM_API)
 
         message = """
         №{} {}
